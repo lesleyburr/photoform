655ca745ede8cc992b8f114c189d93aa1d8933ff
lesleyburr <burrl@bc.edu>
2014-01-10 11:47:14 -0600

    added custom pattern to account number text field and javascript regular expression

---
+++ js/custom-smartform.js
@@ -0,0 +1,7 @@
+$(document)
+  .foundation()
+  .foundation('abide', {
+    patterns: {
+      account-number: /^([0-9][0-9][0-9]-[0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9])$/
+    }
+  });
