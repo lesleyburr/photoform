fa43dbdd4a22a6506816994d8d83e4380c23f81e
lesleyburr <burrl@bc.edu>
2014-01-10 13:59:38 -0600

    updated values in form, added jquery to checkbox inputs to toggle instructions

--- js/custom-smartform.js
+++ js/custom-smartform.js
@@ -1,3 +1,37 @@
+$('#is2dselected').click(function() {
+    $("#txt2dinstructions").toggle(this.checked);
+});
+  
+
+$('#is3dselected').click(function() {
+    $("#txt3dinstructions").toggle(this.checked);
+});
+
+
+$('#isburnselected').click(function() {
+    $("#txtburninstructions").toggle(this.checked);
+});
+
+$('#isotherserviceselected').click(function() {
+    $("#photoservice_other_textarea").toggle(this.checked);
+});
+
+$('#iscollectionsselected').click(function() {
+    $("#photoservice_collections_textarea").toggle(this.checked);
+});
+
+$('#isdeliveryother').click(function() {
+    $("#delivery_other_textarea").toggle(this.checked);
+});
+
+$('#isserverselected').click(function() {
+    $("#folderlocation").toggle(this.checked);
+});
+
+$('#isemailselected').click(function() {
+    $("#e-mail").toggle(this.checked);
+});
+
 // $(document)
 //   .foundation()
 //   .foundation('abide', {
