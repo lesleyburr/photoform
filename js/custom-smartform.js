a2e61404ba6b36e757ce081452427002ea71dc69
lesleyburr <burrl@bc.edu>
2014-01-13 11:19:58 -0600

    added descriptions and additional instructions to photography services options including textarea and datetime input fields

--- js/custom-smartform.js
+++ js/custom-smartform.js
@@ -1,10 +1,57 @@
 $('#is2dselected').click(function() {
     $("#txt2dinstructions").toggle(this.checked);
 });
-  
 
 $('#is3dselected').click(function() {
     $("#txt3dinstructions").toggle(this.checked);
+});
+
+$('#isexhibitiondocumentation').click(function() {
+    $("#exhibitiondocumentation").toggle(this.checked);
+});
+
+$('#isexhibitiondocumentation').click(function() {
+    $("#labelfor_datetime_exhibitiondoc").toggle(this.checked);
+});
+
+$('#isexhibitiondocumentation').click(function() {
+    $("#datetime_exhibitiondoc").toggle(this.checked);
+});
+
+$('#isphotographyexhibitions').click(function() {
+    $("#photographyexhibitions").toggle(this.checked);
+});
+
+$('#isphotographyexhibitions').click(function() {
+    $("#labelfor_datetime_photographyexhibitions").toggle(this.checked);
+});
+
+$('#isphotographyexhibitions').click(function() {
+    $("#datetime_photographyexhibitions").toggle(this.checked);
+});
+
+$('#ismarketingselected').click(function() {
+    $("#marketing").toggle(this.checked);
+});
+
+$('#ismarketingselected').click(function() {
+    $("#labelfor_datetime_marketing").toggle(this.checked);
+});
+
+$('#ismarketingselected').click(function() {
+    $("#datetime_marketing").toggle(this.checked);
+});
+
+$('#iseventselected').click(function() {
+    $("#event").toggle(this.checked);
+});
+
+$('#iseventselected').click(function() {
+    $("#labelfor_datetime_event").toggle(this.checked);
+});
+
+$('#iseventselected').click(function() {
+    $("#datetime_event").toggle(this.checked);
 });
 
 
