9a5ed6089f15b45dc8529e92fdc57cb12453b6eb
lesleyburr <burrl@bc.edu>
2014-01-15 10:40:20 -0600

    added placeholders.min.js in order to fix form placeholders so that they show up in internet explorer

--- js/custom-smartform.js
+++ js/custom-smartform.js
@@ -1,38 +1,18 @@
-// shim for browsers that don't support the html5 placeholder attribute
+$("input[placeholder]").each(function(){
+  if($(this).val()==""){
+    $(this).val($(this).attr("placeholder"));
+    $(this).focus(function(){
+      if($(this).val()==$(this).attr("placeholder")) $(this).val("");
+    });
+    $(this).blur(function(){
+      if($(this).val()==""){
+         $(this).val($(this).attr("placeholder"));
+      }
+     });
+  }
+});
 
-setup_placeholders = (function() {
-    $.support.placeholder = false;
-    test = document.createElement('input');
-    if('placeholder' in test) {
-        $.support.placeholder = true;
-        return function() {}
-    } else {
-        return function(){
-            $(function() {
-                var active = document.activeElement;
-                $('form').delegate(':text', 'focus', function () {
-                    var _placeholder = $(this).attr('placeholder'),
-                        _val = $(this).val();
-                    if (_placeholder != '' && _val == _placeholder) {
-                        $(this).val('').removeClass('hasPlaceholder');
-                    }
-                }).delegate(':text', 'blur', function () {
-                    var _placeholder = $(this).attr('placeholder'),
-                        _val = $(this).val();
-                    // No need to test for values specific to a particular jQuery version
-                    // undefined and an empty string both are falsy
-                    if (!_placeholder && ( _val == '' || _val == _placeholder)) {
-                        $(this).val(_placeholder).addClass('hasPlaceholder');
-                    }
-                }).submit(function () {
-                    $(this).find('.hasPlaceholder').each(function() { $(this).val(''); });
-                });
-                $(':text').blur();
-                $(active).focus();
-            });
-        }
-    }
-})();
+
 
 
 
