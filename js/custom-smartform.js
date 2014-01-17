bc9f565e2975e93411330f5f3f42b8d52b091da7
lesleyburr <burrl@bc.edu>
2014-01-17 14:13:41 -0600

    applied button style to submit button, inserted html meta tag to turn off compatibility mode in IE

--- js/custom-smartform.js
+++ js/custom-smartform.js
@@ -90,3 +90,22 @@ $('#isotheruse').click(function() {
 $('#isexternalclient').click(function() {
     $("#externalclient_name").toggle(this.checked);
 });
+
+
+
+
+
+var initDatepicker = function() {  
+    $('input[type=date]').each(function() {  
+        var $input = $(this);  
+        $input.datepicker({  
+            minDate: $input.attr('min'),  
+            maxDate: $input.attr('max'),  
+            dateFormat: 'yy-mm-dd'  
+        });  
+    });  
+};  
+  
+if(!Modernizr.inputtypes.date){  
+    $(document).ready(initDatepicker);  
+};  
