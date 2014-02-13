bde45a24e1de75f4139e7eb786864381cdded06d
lesleyburr <burrl@bc.edu>
2014-02-13 13:58:41 -0600

    attempting input date field crossbrowser fix

--- js/custom-smartform.js
+++ js/custom-smartform.js
@@ -223,3 +223,27 @@ $('#date').formatter({
   'pattern': '{{99}}-{{99}}-{{9999}}',
   'persistent': true
 });
+
+
+
+
+// 
+// 
+// Crossbrowser 
+// 
+// 
+
+var initDatepicker = function() {
+    $('input[type=date]').each(function() {
+        var $input = $(this);
+        $input.datepicker({
+            minDate: $input.attr('min'),
+            maxDate: $input.attr('max'),
+            dateFormat: 'yy-mm-dd'
+        });
+    });
+};
+ 
+if(!Modernizr.inputtypes.date){
+    $(document).ready(initDatepicker);
+};
