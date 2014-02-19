b426eccfaa1ab28515ca033a698397a5c76b2b77
Enrique Gonzalez <egonzalez78@gmail.com>
2014-02-18 22:51:43 -0600

    tweaking jquery and abide with conditional fields

--- js/custom-smartform.js
+++ js/custom-smartform.js
@@ -17,13 +17,26 @@ $( "#GoToFiles" ).click(function( event ) {
 
 
 
-// CONDITIONAL SECTIONS 
+// CONDITIONAL SECTIONS
   // SERVICES
 
     // NEW IMAGES
-    // Show New Images Menu
+    // SHOW New Images Menu ON CHECK
+    // UNCHECK all checked checkboxes, REMOVE required attribute, HIDE details and HIDE New Images Menu ON UNCHECK
+    // $('#ServicesNew').click(function() {
+    //   $("#ServicesNewMenu").slideToggle(this.checked)
+    //                       .find('input[type=checkbox]:checked').removeAttr('checked')
+    //                       .end()
+    //                       .find(':input')
+    //                       .removeAttr('required');
+    // });
+
     $('#ServicesNew').click(function() {
-      $("#ServicesNewMenu").slideToggle(this.checked);
+      $("#ServicesNewMenu").slideToggle(this.checked)
+                          .find('input[type=checkbox]:checked').removeAttr('checked')
+                          .end()
+                          .find(':input')
+                          .removeAttr('required');
     });
 
     // Show Exhibition Documentation Details
@@ -48,42 +61,69 @@ $( "#GoToFiles" ).click(function( event ) {
                                               </small>\
                                             </div>')
                               .slideToggle( 'slow');
+      $('#PhotoAndImagingRequest').foundation({bindings: 'events'});
     });
 
     // Show Photography for Exhibition Details
     $("#NewPhotoForExhibition").click(function() {
       $( "#NewPhotoForExhibitionDetails" ).html('<div class="field--text">\
-                                    <label for="NewPhotoForExhibitionTitle" class="label">\
-                                        What is the exhibition title?\
-                                        <small>required</small>\
-                                    </label>\
-                                    <input type="text" name="new_photo_for_ex_title" placeholder="Facing Freedom" id="NewPhotoForExhibitionTitle" required>\
-                                    <small class="error">How should I refer to this exhibition?</small>\
-                                </div>\
-                                <div class="field--description">\
-                                    <label class="label" for="NewPhotoForExhibitionDescription">\
-                                        Provide Photography details:\
-                                        <small>required</small>\
-                                        <span data-tooltip class="has-tip tip-right" title="Objects, people, or locations to be photographed."><i class="fi-info"></i></span>\
-                                    </label>\
-                                    <textarea name="new_photo_for_ex_details" placeholder="Photograph the women&rsquo;s suffrage cutouts with people standing among them." id="NewPhotoForExhibitionDescription" required></textarea>\
-                                    <small class="error">Please provide some context on the exhibition.</small>\
-                                </div>\
-                                <div class="field--date">\
-                                    <label for="NewPhotoForExhibitionDate">\
-                                        When should the photography occur? \
-                                        <small>required</small>\
-                                    </label>\
-                                    <input type="date" name="new_photo_for_ex_date" id="NewPhotoForExhibitionDate" required pattern="month_day_year">\
-                                    <small class="error">Use the MM/DD/YYYY date format.</small>\
-                                </div>')
-                              .slideToggle( 'slow');
+                                                    <label for="NewPhotoForExhibitionTitle" class="label">\
+                                                      What is the exhibition title?\
+                                                      <small>required</small>\
+                                                    </label>\
+                                                    <input type="text" name="new_photo_for_ex_title" placeholder="Facing Freedom" id="NewPhotoForExhibitionTitle" required>\
+                                                    <small class="error">How should I refer to this exhibition?</small>\
+                                                  </div>\
+                                                  <div class="field--description">\
+                                                    <label class="label" for="NewPhotoForExhibitionDescription">\
+                                                      Provide Photography details:\
+                                                      <small>required</small>\
+                                                      <span data-tooltip class="has-tip tip-right" title="Objects, people, or locations to be photographed."><i class="fi-info"></i></span>\
+                                                    </label>\
+                                                    <textarea name="new_photo_for_ex_details" placeholder="Photograph the women&rsquo;s suffrage cutouts with people standing among them." id="NewPhotoForExhibitionDescription" required></textarea>\
+                                                    <small class="error">Please provide some context on the exhibition.</small>\
+                                                  </div>\
+                                                  <div class="field--date">\
+                                                    <label for="NewPhotoForExhibitionDate" class="label">\
+                                                      When should the photography occur? \
+                                                      <small>required</small>\
+                                                    </label>\
+                                                    <input type="date" name="new_photo_for_ex_date" id="NewPhotoForExhibitionDate" required pattern="month_day_year">\
+                                                    <small class="error">Use the MM/DD/YYYY date format.</small>\
+                                                  </div>')
+                                    .slideToggle( 'slow');
+      $('#PhotoAndImagingRequest').foundation({bindings: 'events'});
+    });
+
+    // Show Photography for Marketing Details
+    $("#NewPhotoForMarketing").click(function() {
+      $( "#NewPhotoForMarketingDetails" ).html('<div class="field--description">\
+                                                  <label class="label" for="NewPhotoForMarketingDescription">\
+                                                    Provide Photography details:\
+                                                    <small>required</small>\
+                                                    <span data-tooltip class="has-tip tip-right" title="Objects, people, or locations to be photographed."><i class="fi-info"></i></span>\
+                                                  </label>\
+                                                  <textarea name="new_marketing_details" placeholder="Photograph someone in the hot dog suit standing in front of the Pioneer." id="NewPhotoForMarketing" required></textarea>\
+                                                  <small class="error">What would you like photographed?</small>\
+                                                </div>\
+                                                <div class="field--date">\
+                                                  <label for="NewPhotoForMarketingDate" class="label">\
+                                                    When should the photography occur?\
+                                                  <small>required</small>\
+                                                  </label>\
+                                                  <input type="date" name="new_marketing_date" id="NewPhotoForMarketingDate" required pattern="month_day_year">\
+                                                  <small class="error">Use the MM/DD/YYYY date format.</small>\
+                                                </div>')
+                                  .slideToggle( 'slow');
+      $('#PhotoAndImagingRequest').foundation({bindings: 'events'});
     });
 
 
 
     // EXISTING IMAGES
     // Show Existing Images Menu
+    // SHOW New Images Menu ON CHECK
+    // UNCHECK all checked checkboxes and HIDE New Images Menu ON UNCHECK
     $('#ServicesExisting').click(function() {
-      $("#ServicesExistingMenu").slideToggle(this.checked);
+      $("#ServicesExistingMenu").slideToggle(this.checked).find('input[type=checkbox]:checked').removeAttr('checked');
     });
